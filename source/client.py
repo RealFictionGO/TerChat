860eabc8d33ba56856fd076778e0916240f5ebe7
RealFictionGO <76532627+RealFictionGO@users.noreply.github.com>
2022-04-05 20:06:30 +0200

    adding some command line user interface

--- source/client.py
+++ source/client.py
@@ -3,29 +3,65 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-while True:
-    try:
-        ip = input('Input server ip: ')
-        port = int(input('Enter server port: '))
-        break
-    except:
-        print('Invalid input\n Tip: Server port has to be a number')
+def login():
+    while True:
+        try:
+            ip = input('Input server ip: ')
+            port = int(input('Enter server port: '))
+            break
+        except:
+            print('Invalid input\n Tip: Server port has to be a number')
 
-while True:
-    nickname = input('Enter nickname: ')
-    if nickname != '':
-        break
-    else:
-        print('Nickname can\'t be empty')
+    while True:
+        nickname = input('Enter nickname: ')
+        if nickname != '':
+            return ip, port, nickname
+        else:
+            print('Nickname can\'t be empty')
 
-client.connect((ip, port))
+def config():
+    pass
+
+def start():
+        while True:
+            print("""
+   _____          ____ _           _   
+  |_   _|__ _ __ / ___| |__   __ _| |_ 
+    | |/ _ \ '__| |   | '_ \ / _` | __|
+    | |  __/ |  | |___| | | | (_| | |_ 
+    |_|\___|_|   \____|_| |_|\__,_|\__|
+            """)
+            print('Made by RealFiction')
+            print('https://github.com/RealFictionGO')
+            print('https://twitter.com/RealfictionS')
+
+            option = input('>_').strip()
+
+            if option == 'login':
+                l_data = login()
+                return l_data
+            elif option == 'config':
+                config()
+            elif option == 'help' or option == 'ls':
+                print("""
+                
+                | login    - connect to a server 
+                | config   - settings for display
+                | help     - as its name says
+
+                """)
+            else:
+                print('Type \'help\' for command list')
+
+login_data = start()
+client.connect((login_data[0], login_data[1]))
 
 def recive():
     while True:
         try:
             message = client.recv(1024).decode('ascii')
             if message.replace(' ', '') == "NICK":
-                client.send(nickname.encode('ascii'))
+                client.send(login_data[2].encode('ascii'))
             else:
                 print(message)
 
@@ -36,8 +72,8 @@ def recive():
 
 def write():
     while True:
-        message = f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))    
+        message = f'{login_data[2]}: {input("")}'
+        client.send(message.encode('ascii'))
 
 recive_thred = threading.Thread(target=recive)
 recive_thred.start()
--- source/server.py
+++ source/server.py
@@ -1,12 +1,130 @@
 import threading
 import socket
+import os
+import json
 
-host = '127.0.0.1'
-port = int(input("Enter socket for chatroom: "))
-server_name = input("Name the server ")
+from source.client import config, login  
+
+host = socket.gethostname()  
+path = os.getcwd()
+
+#search server directory
+def server_dir():
+    if os.path.exists(path + "\\rooms"):
+        os.chdir(path + "\\rooms")
+    else:
+        os.mkdir('rooms')
+        os.chdir(path + "\\rooms")
+    return path + "\\rooms"
+
+# mode choosing [1] loading already saved server from \rooms dir
+#               [2] creating new server save
+
+def print_servers(print:bool):
+    server_dir()
+    if print == True:
+        print('________________________________________')
+        for server in os.listdir():
+            print(server.replace('.json', ''))
+        print('________________________________________')
+
+    server_names = os.listdir()
+    for name in server_names:
+        raw_name = name.replace('.json','')
+        index = server_names.index(name)
+        server_names.pop(index)
+        server_names.insert(index, raw_name)
+
+    return server_names
+
+def load_server():
+    all_servers = print_servers()
+
+    while True:
+        l_server = input('\n Which server to load? \nserver name: ').strip()
+
+        if l_server in all_servers:
+            with open(l_server + '.json', 'r') as f:
+                server_data = json.loads(f)
+                f.close()
+            
+            return server_data
+
+        elif l_server == 'exit':
+            return None
+
+        else:
+            print('Choose server from a list')
+
+def create_server():
+    server_dir()
+    print_servers(False)
+    new_server_data = {}
+    while True:
+        name = input('New server name: ')
+        if name not in os.listdir():
+            break
+        else:
+            print('Server name already exist')
+
+    new_server_data.update({'name' : name})
+    while True:
+        try:
+            server_port = int(input('Server\'s port ( recomended 30000 > ): ').strip())
+            break
+        except:
+            print('You have to type a number')
+    new_server_data.update({'port' : server_port})
+
+    with open(name + '.json', 'w') as f:
+        f.write(json.dumps(new_server_data))
+        f.close()
+
+    while True:
+        accept = input('Do you want to load this server? [y/n]\n ').strip()
+        if accept == 'y':
+            return new_server_data
+        elif accept == 'n':
+            return
+        else:
+            print('You have to type \'y\' or \'n\'')
+
+def start():
+    while True:
+        print("""
+_____           ____ _           _   
+|_   _|__ _ __ / ___| |__   __ _| |_ 
+  | |/ _ \ '__| |   | '_ \ / _` | __|
+  | |  __/ |  | |___| | | | (_| | |_ 
+  |_|\___|_|   \____|_| |_|\__,_|\__|
+        """)
+        print('Made by RealFiction')
+        print('https://github.com/RealFictionGO')
+        print('https://twitter.com/RealfictionS')
+
+        option = input('>_').strip()
+
+        if option == 'load':
+            l_data = load_server()
+            return l_data
+        elif option == 'create':
+            c_data = create_server()
+            return c_data
+        elif option == 'help' or option == 'ls':
+            print("""
+            
+            | load     - connect to a server 
+            | create   - settings for display
+            | help     - as its name says
+
+            """)
+        else:
+            print('Type \'help\' for command list')
+
+server_data = start()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
+server.bind((host, server_data.get('port')))
 server.listen()
 
 clients = []
@@ -42,11 +160,11 @@ def recive():
         clients.append(client)
         print(f'New client connected {nickname}')
         broadcast(f'{nickname} joined chat'.encode('ascii'))
-        client.send(f'Connected to {server_name}'.encode('ascii'))
+        client.send(f'Connected to {server_data.get("name")}'.encode('ascii'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
 print("Server is active")
-print(f'Welcome to {server_name}')
+print(f'Welcome to {server_data.get("name")}')
 recive()
